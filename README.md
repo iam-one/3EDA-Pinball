69ad23442338a6b37df4278d3d0544fdaac3f71d
one <imone075@gmail.com>
2023-07-22 16:44:03 +0900

    add readme

---
+++ README.md
@@ -0,0 +1,11 @@
+### SEDU Project -  PINBALL
+
+---
+
+물리공학 동아리 SEDA 자연숲 과학교실 프로젝트
+
+> 아두이노 개념과 활용에 대한 공학적 소양 증진을 목적으로 함.  
+> (중학생 대상)
+
+<img src="/public/Sedu_Pinball_Circuit_IMG.png">
+
--- sedu-pinball.ino
+++ sedu-pinball.ino
@@ -1,10 +1,13 @@
-// declare pin value
-int l_servo = 6; // D3
-int r_servo = 8; // D5
-int ir_in = 5; // D2
-int ir_out = 19; // A0
-int l_tact = 7; // D4
-int r_tact = 9; // D6
+// declare default pin value
+const int l_servo = 6; // D3
+const int r_servo = 8; // D5
+const int cds_in = 19; // A0
+const int l_tact = 7; // D4
+const int r_tact = 9; // D6
+
+// define seg pin value
+const byte pin_segs[8] = {14, 15, 20, 21, 22, 23, 24, 25}; // D11 (a), D12 (b), A1 (c), A2 (d), A3 (e), A4 (f), A5 (g), A6 (dp)
+const byte pin_segDigit[4] = {10, 11, 12, 13}; // D7 (d1), D8 (d2), D9 (d3), D10 (d4)
 
 // define seg value
 const byte segNum[10][7] = {
@@ -20,18 +23,19 @@ const byte segNum[10][7] = {
    {1,1,1,1,0,1,1}  // 9
 };
 
-
-// define seg pin value
-byte pin_segs[8] = {}; // a, b, c, d, e, f, g, dp
-byte pin_segDigit[4] = {}; // d1, d2, d3, d4
-
 void setup() { // setup
+  pinMode(l_tact, INPUT_PULLUP);
+  pinMode(r_tact, INPUT_PULLUP);
+  pinMode(l_servo, OUTPUT);
+  pinMode(r_servo, OUTPUT);
+  pinMode(cds_in, INPUT_PULLUP);
+
   for(int i=0; i<8; i++){
-    pinMode(segPin[i], OUTPUT);
+    pinMode(pin_segs[i], OUTPUT);
   }
   for(int j=0; j<4; j++){
-    pinMode(digitPin[j], OUTPUT);
-    digitalWrite(digitPin[j], HIGH);
+    pinMode(pin_segDigit[j], OUTPUT);
+    digitalWrite(pin_segDigit[j], HIGH);
   }
 }
 
@@ -40,17 +44,17 @@ void loop() {
 
 void segClear(){ // segment clear
   for(int i=0;i<8;i++){
-    digitalWrite(segPin[i], LOW);        
+    digitalWrite(pin_segs[i], LOW);
   }
 }
 
 void segOut(int d, int value, int dp){ // segment output
   segClear();
-  digitalWrite(pin_segDigit[d], LOW); 
+  digitalWrite(pin_segDigit[d], LOW);
   for(int i=0;i<7;i++){
-     digitalWrite(segPin[i], segValue[value][i]);        
+     digitalWrite(pin_segs[i], segNum[value][i]);
   }
-  digitalWrite(segPin[7], dp);
+  digitalWrite(pin_segs[7], dp);
   delayMicroseconds(1000);
-  digitalWrite(pin_segDigit[d], HIGH); 
+  digitalWrite(pin_segDigit[d], HIGH);
 }
